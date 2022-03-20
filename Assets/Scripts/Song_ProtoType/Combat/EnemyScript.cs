edda28350465b1a8e245d398da2e7e44396268a9
LodingMan <nicerice3179@gmail.com>
2022-03-20 20:36:25 +0900

    CombatUpload

---
+++ Assets/Scripts/Song_ProtoType/Combat/EnemyScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject SelectChild;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Song_ProtoType/Combat/PlayerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int speed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
