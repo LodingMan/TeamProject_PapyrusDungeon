596f178bd5343da17a6f446df46011bb696488c8
jaehoon0185 <jaehoon0185@gmail.com>
2022-03-27 18:04:53 +0900

    Town프리팹 정리 & 비활성화

---
+++ Assets/Scenes/Shin/StartLoadCurrentHeroData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLoadCurrentHeroData : MonoBehaviour
+{
+    public Song.HeroManager heroManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heroManager = GameObject.Find("HeroManager").GetComponent<Song.HeroManager>();
+        heroManager._Load();
+    }
+}
