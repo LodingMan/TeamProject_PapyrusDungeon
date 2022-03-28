b6c07e3c97cc5153dc2cb59f7e7ca7a5199a221e
jaehoon0185 <jaehoon0185@gmail.com>
2022-03-28 17:11:45 +0900

    NaviMesh완료.

--- Assets/Scripts/Town/NaviMeshHero.cs
+++ Assets/Scripts/Town/NaviMeshHero.cs
@@ -3,48 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NaviMeshHero : MonoBehaviour
+namespace Shin
 {
-    public Transform target;
-    public Transform[] point;
-    NavMeshAgent agent;
-    public float dist;
-    int rnd_zen, rnd_target;
-    public bool isMove;
-    public float curTime, coolTime;
-    void Start()
+    public class NaviMeshHero : MonoBehaviour
     {
-        agent = GetComponent<NavMeshAgent>();
-        // Hierarchy에서 NaviPoint0@을 찾아서 그 수만큼 point 배정.
-
-        rnd_zen = Random.Range(0, point.Length);
-        rnd_target = Random.Range(0, point.Length);
-        gameObject.transform.position = point[rnd_zen].position;
-        target = point[rnd_target];
-    }
-
-
-    void Update()
-    {
-        dist = Vector3.Distance(transform.position, target.position);
-        if (dist > 1.1f)
+        public Transform target;
+        public Transform[] point;
+        NavMeshAgent agent;
+        public float dist;
+        int rnd_zen, rnd_target;
+        public bool isMove;
+        public float curTime, coolTime;
+        string nodeName;
+        public int pointCnt;
+        void Start()
         {
-            isMove = true;
-            agent.SetDestination(target.position);
-            coolTime = Random.Range(1.5f, 2.5f);
+            agent = GetComponent<NavMeshAgent>();
+            point = new Transform[pointCnt = 11];
+
+            InitNaviPoint(); // hierarchy에 있는 NaviPoint를 point에 배정하는 함수.
+            
+            rnd_zen = Random.Range(0, point.Length);
+            rnd_target = Random.Range(0, point.Length);
+            gameObject.transform.position = point[rnd_zen].position;
+            target = point[rnd_target];
         }
-        else
+
+        void Update()
         {
-            isMove = false;
-            curTime += Time.deltaTime;
-            if (curTime > coolTime)
+            dist = Vector3.Distance(transform.position, target.position);
+            if (dist > 1.1f)
             {
-                curTime = 0;
-                rnd_target = Random.Range(0, point.Length);
-                target = point[rnd_target];
+                isMove = true;
+                agent.SetDestination(target.position);
+                coolTime = Random.Range(1.5f, 2.5f);
+            }
+            else
+            {
+                isMove = false;
+                curTime += Time.deltaTime;
+                if (curTime > coolTime)
+                {
+                    curTime = 0;
+                    rnd_target = Random.Range(0, point.Length);
+                    target = point[rnd_target];
+                }
+            }
+
+        }
+
+        public void InitNaviPoint() // Hierarchy에서 NaviPoint검색해서 point에 할당.
+        {
+            for (int i = 0; i < pointCnt; i++)
+            {
+                if ((GameObject.Find("NaviPoint" + i).GetComponent<Transform>() == true))
+                {
+                    point[i] = GameObject.Find("NaviPoint" + i).GetComponent<Transform>();
+                }
             }
         }
-
     }
-
 }
+
