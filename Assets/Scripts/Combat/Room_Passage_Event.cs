82f6034ee4d8a672f8d178de69eb15f80a77a6fa
LodingMan <nicerice3179@gmail.com>
2022-04-05 16:21:33 +0900

    Marge

--- Assets/Scripts/Combat/Room_Passage_Event.cs
+++ Assets/Scripts/Combat/Room_Passage_Event.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class Room_Passage_Event : MonoBehaviour
 {
+    public Combat_Event_UI_Manager combat_Event_UI_Manager;
 
 
     public void Passage_HpDown_Event()
     {
+        combat_Event_UI_Manager.Yes_No_Button[0].onClick.AddListener(Party_Hp_Down);
+        combat_Event_UI_Manager.Yes_No_Button[1].onClick.AddListener(None_Event);
+    }
+    public void Party_Hp_Down()
+    {
         Debug.Log("모든 맴버의 HP가 감소함!");
     }
+
+
+
+
+
+
+
+
+
+
+    public void None_Event()
+    {
+        Debug.Log("아무일도 일어나지 않음");
+    }
+
+
 }
