433533a31d579c6d846c8130445c45ecface67b3
jaehoon0185 <jaehoon0185@gmail.com>
2022-04-06 11:55:55 +0900

    12345

--- Assets/Scripts/Hero&Enemy/Management/HeroManager.cs
+++ Assets/Scripts/Hero&Enemy/Management/HeroManager.cs
@@ -30,7 +30,7 @@ namespace Song
 
         int JobSkillStartIndex; // 0�̸� 0~4�� �ε��� , 5��� 5������ 9�������� �ε����� for������ ������ ��ų���� ������, �ϴ� for�� ����
         public int HeroColor;
-
+        string heroName = "BasicName";
 
         public GuildManager guildManager;
 
@@ -41,23 +41,76 @@ namespace Song
                 string rndJob = name_JobTable.RandomJobTable[Random.Range(0, name_JobTable.RandomJobTable.Length)]; //������ ���� �̸� ������
                 int rndColor = Random.Range(0, 4);
                 FirstHeroCreate(rndJob, rndColor);
+                
+            }
+            // 체크용 리스트 remove 해주고, 중복처리용 리스트를 다시 add.
+            name_JobTable.CurrentHeroName.RemoveRange(0, name_JobTable.CurrentHeroName.Count); // 일단 전부 지우고
+            for (int i = 0; i < CurrentHeroList.Count; i++)
+            {
+                name_JobTable.CurrentHeroName.Add(CurrentHeroList[i].name);
             }
 
             guildManager.UnemployedHero_UI_Prefabs_UpLoad(unemployedHeroList);
 
         }
 
-
-        public void FirstHeroCreate(string HeroJobName, int color)  //�������� �ʱⰪ�� HeroObject�� �����ϴ� �Լ�. 
+        public void CheckOverName(string randomName)
         {
-            string RandomName = name_JobTable.RandomNameTable[Random.Range(0, name_JobTable.RandomNameTable.Length)]; //������ ������Ʈ�� ������ �̸� �����
+            bool isOver = false;
+
+            if(name_JobTable.CurrentHeroName.Count != 0) // 리스트에 하나라도 들어있으면 중복검사 해야함.
+            {
+                for (int i = 0; i < name_JobTable.CurrentHeroName.Count; i++)
+                {
+                    if (randomName == name_JobTable.CurrentHeroName[i])
+                    {
+                        isOver = true;
+                    }
+                }
+
+                if (!isOver) // 중복이 아니면, 여기서 CurrentHeroList를 검사해야함.
+                {
+                    bool isCur = false;
+                    for (int i = 0; i < CurrentHeroList.Count; i++)
+                    {
+                        if (randomName == CurrentHeroList[i].name)
+                        {
+                            isCur = true;
+                        }
+                    }
+                    if (isCur == false)
+                    {
+                        heroName = randomName;
+                        name_JobTable.CurrentHeroName.Add(heroName);
+                        Debug.Log(heroName);
+                    }
+                }
+                else
+                {
+                    randomName = name_JobTable.RandomNameTable[Random.Range(0, name_JobTable.RandomNameTable.Length)]; // 랜덤으로 이름 받아옴.
+                    CheckOverName(randomName);
+                }
+            }
+        }
+
+        public void FirstHeroCreate(string heroJob, int color)  //�������� �ʱⰪ�� HeroObject�� �����ϴ� �Լ�. 
+        {
+            string randomName = name_JobTable.RandomNameTable[Random.Range(0, name_JobTable.RandomNameTable.Length)]; // 랜덤으로 이름 받아옴.
+            if (name_JobTable.CurrentHeroName.Count == 0) // 맨처음 중복처리 필요없을 때.
+            {
+                heroName = randomName;
+                name_JobTable.CurrentHeroName.Add(heroName);
+                Debug.Log(heroName);
+            }
+            CheckOverName(randomName);
+
             // 중복처리 해야함.
             #region //������ �������� ���� �ο� ������Ʈ ����, ��ų �ε��� �ο�
-            switch (HeroJobName)
+            switch (heroJob)
             {
                 case "Mage":
 
-                    initStat = new Stat(0, RandomName, "Mage", 25, 25, 50, 50, 7, 5, 0, 90, 10, 3); //기본스텟이 빈약하지만 스킬딜이 강력한 컨셉 
+                    initStat = new Stat(0, heroName, "Mage", 25, 25, 50, 50, 7, 5, 0, 90, 10, 3); //기본스텟이 빈약하지만 스킬딜이 강력한 컨셉 
                     CurrentCreateHero = Instantiate(HeroPrefabs[color]) as GameObject; //������ ����
                     CurrentCreateHero.name = initStat.Name; // ������ ������Ʈ�� ���� �̸�
 
@@ -65,7 +118,7 @@ namespace Song
                     break;
                 case "Archer":
 
-                    initStat = new Stat(1, RandomName, "Archer", 28 , 28 , 20 , 20, 15, 8, 45, 90, 30, 10); //속도빠름, 회피높고 크리티컬 높음
+                    initStat = new Stat(1, heroName, "Archer", 28 , 28 , 20 , 20, 15, 8, 45, 90, 30, 10); //속도빠름, 회피높고 크리티컬 높음
                     CurrentCreateHero = Instantiate(HeroPrefabs[color+4]) as GameObject; //�ü� ����
                     CurrentCreateHero.name = initStat.Name; // ������ ������Ʈ�� ���� �̸�
 
@@ -73,7 +126,7 @@ namespace Song
                     break;
                 case "Babarian":
 
-                    initStat = new Stat(2, RandomName, "Babarian", 40, 40, 20, 20, 32, 12, 20, 95, 10, 7); //HP 준수 공격력 높음
+                    initStat = new Stat(2, heroName, "Babarian", 40, 40, 20, 20, 32, 12, 20, 95, 10, 7); //HP 준수 공격력 높음
                     CurrentCreateHero = Instantiate(HeroPrefabs[color+8]) as GameObject;
                     CurrentCreateHero.name = initStat.Name;
 
@@ -82,7 +135,7 @@ namespace Song
                     break;
                 case "Knight":
 
-                    initStat = new Stat(3, RandomName, "Knight", 53, 53 , 30 , 30 , 15, 18, 5, 100, 7, 4);
+                    initStat = new Stat(3, heroName, "Knight", 53, 53 , 30 , 30 , 15, 18, 5, 100, 7, 4);
                     CurrentCreateHero = Instantiate(HeroPrefabs[color+12]) as GameObject;
                     CurrentCreateHero.name = initStat.Name;
 
@@ -90,7 +143,7 @@ namespace Song
                     break;
                 case "Barristan":
 
-                    initStat = new Stat(4, RandomName, "Barristan", 60 , 60 , 20, 20 , 12, 21, 5, 85, 5, 2);
+                    initStat = new Stat(4, heroName, "Barristan", 60 , 60 , 20, 20 , 12, 21, 5, 85, 5, 2);
                     CurrentCreateHero = Instantiate(HeroPrefabs[color+16]) as GameObject;
                     CurrentCreateHero.name = initStat.Name;
 
@@ -98,7 +151,7 @@ namespace Song
                     break;
                 case "Porter":
 
-                    initStat = new Stat(5, RandomName, "Porter", 33, 33 , 33, 33 , 3, 13, 33, 83, 23, 3);
+                    initStat = new Stat(5, heroName, "Porter", 33, 33 , 33, 33 , 3, 13, 33, 83, 23, 3);
                     CurrentCreateHero = Instantiate(HeroPrefabs[color+20]) as GameObject;
                     CurrentCreateHero.name = initStat.Name;
 
