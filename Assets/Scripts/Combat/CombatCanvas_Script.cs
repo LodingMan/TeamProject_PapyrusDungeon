c98f7cb79fcd1a8305164ee01fb200f83d978494
jaehoon0185 <jaehoon0185@gmail.com>
2022-04-07 12:42:53 +0900

    123

--- Assets/Scripts/Combat/CombatCanvas_Script.cs
+++ Assets/Scripts/Combat/CombatCanvas_Script.cs
@@ -7,7 +7,9 @@ public class CombatCanvas_Script : MonoBehaviour
 
     private void Awake()
     {
-        //Screen.SetResolution(1280, 1280 / 16*9, true);
+        //Screen.SetResolution(1280, 1280 / 16 * 9, true);
+
+        //Screen.SetResolution(1280, 1280 / 16 * 9, false);
 
 
     }
