e666a00dd2e804cd6a0e710bdc3784541c1edfdd
LodingMan <nicerice3179@gmail.com>
2022-04-07 13:09:18 +0900

    comit1

--- Assets/Scripts/Combat/CombatCanvas_Script.cs
+++ Assets/Scripts/Combat/CombatCanvas_Script.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CombatCanvas_Script : MonoBehaviour
 {
 
-    private void Awake()
+    public Camera CombatCamera;
+    public Camera TentCamera;
+    private void Start()
     {
-        //Screen.SetResolution(1280, 1280 / 16 * 9, true);
-
-        //Screen.SetResolution(1280, 1280 / 16 * 9, false);
 
 
+      //  SetResolution(); 
     }
+    //public void SetResolution()
+    //{
+    //    int setWidth = 1280; // ����� ���� �ʺ�
+    //    int setHeight = 720; // ����� ���� ����
+
+    //    int deviceWidth = Screen.width; // ��� �ʺ� ����
+    //    int deviceHeight = Screen.height; // ��� ���� ����
+
+    //    Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution �Լ� ����� ����ϱ�
+
+    //    if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // ����� �ػ� �� �� ū ���
+    //    {
+    //        float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // ���ο� �ʺ�
+    //        Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // ���ο� Rect ����
+    //        CombatCamera.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // ���ο� Rect ����
+    //        TentCamera.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // ���ο� Rect ����
+
+    //    }
+    //    else        
+    //    {
+    //        float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // ���ο� ����
+    //        Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // ���ο� Rect ����
+    //        CombatCamera.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // ���ο� Rect ����
+    //        TentCamera.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // ���ο� Rect ����
+    //    }
+    //    Screen.fullScreen = false;
+
+    //}
 
 
 }
+
