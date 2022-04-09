36e0223b5f0220d1707799f5a994fcdaf5ef2a25
random0v0 <96767622+random0v0@users.noreply.github.com>
2022-04-09 19:48:06 +0900

    11

--- Assets/Scripts/Combat/Combat_Effect_Manager.cs
+++ Assets/Scripts/Combat/Combat_Effect_Manager.cs
@@ -9,17 +9,38 @@ public class Combat_Effect_Manager : MonoBehaviour
     public e_CombatManager combatMgr;
     public List<GameObject> heroEffects = new List<GameObject>();
     public List<GameObject> enemyEffects = new List<GameObject>();
+    public List<int> buffSkillIndex = new List<int>(); //버프스킬 확인
 
+    public bool isBuffSkill = false;
 
 
 
     public void HeroEffect_On(GameObject target)
     {
+        for (int i = 0; i < buffSkillIndex.Count; i++)
+        {
+            if (combatMgr.SaveSkill.Index == buffSkillIndex[i])
+            {
+                isBuffSkill = true;
+                break;
+            }
+        }
 
-        GameObject effectHero = Instantiate(heroEffects[combatMgr.SaveSkill.Index]);
-        effectHero.transform.position = target.transform.position + new Vector3(0, 0, -1);
-        effectHero.transform.DOMove(effectHero.transform.position - new Vector3(-0.5f, 0f, 0f), 3f).SetLink(effectHero,LinkBehaviour.KillOnDestroy);
-        Destroy(effectHero, 3f);
+        if (!isBuffSkill)
+        {
+            GameObject effectHero = Instantiate(heroEffects[combatMgr.SaveSkill.Index]);
+            effectHero.transform.position = target.transform.position + new Vector3(0, 0, -1);
+            effectHero.transform.DOMove(effectHero.transform.position - new Vector3(-0.5f, 0f, 0f), 3f).SetLink(effectHero, LinkBehaviour.KillOnDestroy);
+            Destroy(effectHero, 3f);
+        }
+        else
+        {
+            GameObject effectHero2 = Instantiate(heroEffects[combatMgr.SaveSkill.Index]);
+            effectHero2.transform.position = target.transform.position + new Vector3(0, 0, -1);
+            effectHero2.transform.DOMove(effectHero2.transform.position - new Vector3(-0.5f, 0f, 0f), 3f).SetLink(effectHero2, LinkBehaviour.KillOnDestroy);
+            Destroy(effectHero2, 3f);
+        }
+
 
 
 
