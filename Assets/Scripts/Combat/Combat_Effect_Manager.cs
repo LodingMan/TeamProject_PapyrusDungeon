f08e224104ad2a4a75deb0835a8a3149805c173f
random0v0 <96767622+random0v0@users.noreply.github.com>
2022-04-10 18:52:53 +0900

    22

--- Assets/Scripts/Combat/Combat_Effect_Manager.cs
+++ Assets/Scripts/Combat/Combat_Effect_Manager.cs
@@ -10,6 +10,9 @@ public class Combat_Effect_Manager : MonoBehaviour
     public List<GameObject> heroEffects = new List<GameObject>();
     public List<GameObject> enemyEffects = new List<GameObject>();
     public List<int> buffSkillIndex = new List<int>(); //버프스킬 확인
+    public List<AudioClip> sfx = new List<AudioClip>(); //스킬 사운드
+    public List<AudioClip> eSfx = new List<AudioClip>(); //적 스킬 사운드
+    public AudioSource audioSource;
 
     public bool isBuffSkill = false;
 
@@ -38,6 +41,7 @@ public class Combat_Effect_Manager : MonoBehaviour
         {
             GameObject effectHero = Instantiate(heroEffects[combatMgr.SaveSkill.Index]);
             effectHero.transform.position = target.transform.position + new Vector3(0, 0, -1);;
+            audioSource.PlayOneShot(sfx[combatMgr.SaveSkill.Index]);
             effectHero.transform.DOMove(effectHero.transform.position - new Vector3(-0.8f, 0f, 0f), 3f).SetLink(effectHero, LinkBehaviour.KillOnDestroy);
             Destroy(effectHero, 3f);
         }
@@ -45,6 +49,7 @@ public class Combat_Effect_Manager : MonoBehaviour
         {
             GameObject effectHero2 = Instantiate(heroEffects[combatMgr.SaveSkill.Index]);
             effectHero2.transform.position = hero.transform.position + new Vector3(0, 0, -1);
+            audioSource.PlayOneShot(sfx[combatMgr.SaveSkill.Index]);
             effectHero2.transform.DOMove(effectHero2.transform.position - new Vector3(-1.4f, 0f, 0f), 3f).SetLink(effectHero2, LinkBehaviour.KillOnDestroy);
             Destroy(effectHero2, 3f);
         }
@@ -58,6 +63,7 @@ public class Combat_Effect_Manager : MonoBehaviour
     {
         GameObject effectEnemy = Instantiate(enemyEffects[0]);
         effectEnemy.transform.position = target.transform.position + new Vector3(0, 1, -1);
+        audioSource.PlayOneShot(eSfx[combatMgr.SaveSkill.Index]);
         Destroy(effectEnemy, 3f);
 
     }
