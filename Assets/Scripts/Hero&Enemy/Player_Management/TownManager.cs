990bfd73ce40817de24e327988a342e0770fd0ee
random0v0 <96767622+random0v0@users.noreply.github.com>
2022-04-11 09:34:09 +0900

    11

--- Assets/Scripts/Hero&Enemy/Player_Management/TownManager.cs
+++ Assets/Scripts/Hero&Enemy/Player_Management/TownManager.cs
@@ -20,6 +20,7 @@ public class TownManager : MonoBehaviour
     public ShopManager shopMgr;
     public Shin.UI_DungeonInitButton DIB;
     public Song.UI_DungeonSelect_Manager uI_DungeonSelect_Manager;
+    public IntroSceneScript introSceneScript;
 
     public List<GameObject> OnControll = new List<GameObject>(); 
 
@@ -35,6 +36,7 @@ public class TownManager : MonoBehaviour
 
     private void Awake()
     {
+        introSceneScript = GameObject.Find("BGM_Manager").GetComponent<IntroSceneScript>();
         isTown = true;
         isTent = false;
         isCombat = false;
@@ -155,6 +157,9 @@ public class TownManager : MonoBehaviour
         isTown = true;
         isTent = false;
         isCombat = false;
+        introSceneScript.audioSS.clip = introSceneScript.audioTown;
+        introSceneScript.audioSS.Play();
+        
 
         DIB.dgMgr.isTent = false;
         DIB.TownPrefabs.SetActive(true);
