c36125108cd32e658b96f76df61c8984f151159b
random0v0 <96767622+random0v0@users.noreply.github.com>
2022-04-16 22:17:28 +0900

    일본어 추가

--- Assets/Scripts/Hero&Enemy/Table/SkillDetailTable.cs
+++ Assets/Scripts/Hero&Enemy/Table/SkillDetailTable.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using Lean.Localization;
 
-namespace Shin {
+namespace Shin
+{
     // SkillDetailManager에 달아 놓고, 스킬 설명과 스킬 이미지 저장용.
     public class SkillDetailTable : MonoBehaviour
     {
         public GameObject leanlocalzation;
         List<Dictionary<string, object>> dataKOR;
         List<Dictionary<string, object>> dataENG;
+
+        List<Dictionary<string, object>> dataJPN;
         public string currentLanguage;
         public List<SkillDetail> skilldetails = new List<SkillDetail>();
         public Sprite[] sprite;
-        
+
         private void Start()
         {
             dataKOR = CSVReader.Read("SkillDetailKorCSV");
             dataENG = CSVReader.Read("SkillDetailEngCSV");
+            dataJPN = CSVReader.Read("SkillDetailJpnCSV");
 
             currentLanguage = leanlocalzation.GetComponent<LeanLocalization>().DefaultLanguage;
             skilldetails.Add(new SkillDetail(0, "None", "None"));
@@ -36,9 +40,16 @@ namespace Shin {
                     skilldetails.Add(new SkillDetail(i, dataENG[i - 1]["Name"].ToString(), dataENG[i - 1]["Effect"].ToString()));
                 }
             }
-            
+            else if (currentLanguage == "Japanese")
+            {
+                for (var i = 1; i < dataKOR.Count + 1; i++)
+                {
+                    skilldetails.Add(new SkillDetail(i, dataJPN[i - 1]["Name"].ToString(), dataJPN[i - 1]["Effect"].ToString()));
+                }
+            }
 
-            
+
+
         }
         public void ChangeLanguage()
         {
@@ -61,6 +72,13 @@ namespace Shin {
                 for (var i = 1; i < dataENG.Count + 1; i++)
                 {
                     skilldetails.Add(new SkillDetail(i, dataENG[i - 1]["Name"].ToString(), dataENG[i - 1]["Effect"].ToString()));
+                }
+            }
+            else if (currentLanguage == "Japanese")
+            {
+                for (var i = 1; i < dataJPN.Count + 1; i++)
+                {
+                    skilldetails.Add(new SkillDetail(i, dataJPN[i - 1]["Name"].ToString(), dataJPN[i - 1]["Effect"].ToString()));
                 }
             }
         }
